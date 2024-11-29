f5bd161c532540ac87757c34bdb674e67522d351
Souron <souravbachhar560@gmail.com>
2024-11-29 20:49:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,18 +2,18 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7865697303:AAFpJfdFpS8nre-npWS5ANli7pamvyjQGJM")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", ""))
-    API_HASH = os.environ.get("API_HASH" "")
+    APP_ID = int(os.environ.get("APP_ID", "25917332"))
+    API_HASH = os.environ.get("API_HASH" "e602b8e0ec123b7d58dcb70c31703664")
     # Update channel for Force Subscribe
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "cartoon_seriesz")
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "AmorousSouls")
     # log channel
-    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002405642627")
     # Get these values from my.telegram.org
     CHAT_ID = os.environ.get("CHAT_ID", "")
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "677682427").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5897395932").split())
     # Banned Unwanted Members..
     BANNED_USERS = []
     # the download location, where the HTTP Server runs
@@ -38,5 +38,5 @@ class Config(object):
     # watermark file
     DEF_WATER_MARK_FILE = ""
     # Database url
-    DB_URI = os.environ.get("DATABASE_URL", "postgres://wmynzqfvxqvhah:78b6b1191cd6d4ae196239a2e091d43cff30e5fe6bbbec7b48e683ff8b57ec3e@ec2-54-85-80-92.compute-1.amazonaws.com:5432/dbuuofqgegto4u")
+    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://orionoshi:seByEMSqlnnlmWKc@cluster0.13mls.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     
